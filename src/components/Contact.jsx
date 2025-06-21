35ae77efd3ac129ac6285e7b93fb04d561bff5f2
Elena Gregov <124687938+ElenaGregov@users.noreply.github.com>
2025-06-21 16:07:20 +0200

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,14 +7,14 @@ const Contact = () => {
   return (
     <section
       id="contact"
-      className="section py-20 px-4 bg-neutral-400 dark:bg-gray-900 text-center"
+      className="section py-20 px-4 bg-neutral-400 text-center"
     >
       <FadeInWhenVisible>
         <div className="mb-12 text-center">
-          <h2 className="text-4xl md:text-5xl font-bold tracking-tight text-rose-500 dark:text-white relative inline-block">
+          <h2 className="text-4xl md:text-5xl font-bold tracking-tight text-rose-500 relative inline-block">
             <span className="relative z-10">Contact me</span>
           </h2>
-          <p className="mt-8 text-lg text-gray-700 dark:text-gray-400 max-w-xl mx-auto">
+          <p className="mt-8 text-lg text-gray-700 max-w-xl mx-auto">
             Feel free to reach out for collaboration or questions
           </p>
         </div>
